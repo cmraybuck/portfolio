bfcbc63b4f8ac5fe988a9727a213649f5295ad58
Micah Raybuck <micah@MacBook-Pro.lan>
2022-06-18 15:49:41 -0400

    initial commit

---
+++ dice game/index.js
@@ -0,0 +1,27 @@
+
+let randomNumber1 = Math.floor(Math.random() * 6) + 1; //1-6
+
+let randomDiceImage = `./media/dice${randomNumber1}.png`; //dice1.png - dice6.png
+
+let image1 = document.querySelectorAll("img")[0];
+
+image1.setAttribute("src", randomDiceImage);
+
+
+let randomNumber2 = Math.floor(Math.random() * 6) + 1;
+
+let randomImageSource2 = `./media/dice${randomNumber2}.png`;
+
+document.querySelectorAll("img")[1].setAttribute("src", randomImageSource2);
+
+
+//If player 1 wins
+if (randomNumber1 > randomNumber2) {
+  document.querySelector("h1").innerHTML = "Player 1 Wins!";
+}
+else if (randomNumber2 > randomNumber1) {
+  document.querySelector("h1").innerHTML = "Player 2 Wins!";
+}
+else {
+  document.querySelector("h1").innerHTML = "Draw!";
+}
